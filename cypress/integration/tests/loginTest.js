5bbd5021d21c8f5c8fd5cf64e1965dc1f0602f87
Brayan Ulloa Martinez <brayan.ulloa@Brayan-Ulloa-MacBook-Pro.local>
2021-09-21 15:10:57 -0600

    Adding Logout test

--- cypress/integration/tests/loginTest.js
+++ cypress/integration/tests/loginTest.js
@@ -7,6 +7,10 @@ describe('Login Tests', () => {
     })
 
     it('Verify that the login functionality works as expected', () => {
-        loginPage.submitUsernameAndPassword('auto','auto')
+        loginPage.submitUsernameAndPassword('auto', 'auto')
+    })
+
+    it('Verify that the logout functionality works as expected', () => {
+        loginPage.clickLogoutOption('auto', 'auto')
     })
 })
--- cypress/support/page_objects/loginPage.js
+++ cypress/support/page_objects/loginPage.js
@@ -7,6 +7,18 @@ export class LoginPage {
         cy.contains('Log in').click()
         cy.contains('Logout').should('contain', 'Logout')
     }
+
+    clickLogoutOption(username, password){
+        cy.contains('My account').click()
+        cy.get('#username').type(username)
+        cy.get('#password').type(password)
+        cy.contains('Log in').click()
+        cy.contains('Logout').click({force: true})
+        cy.get('.site-title').should('contain', 'Automation Playground')
+    }
+    
+
+
 }
 
 export const loginPage = new LoginPage()
